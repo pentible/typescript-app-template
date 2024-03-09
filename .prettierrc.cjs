e719384caf92ba0c5ea65e95271612fc0f427e10
William Villeneuve <ciiqr@users.noreply.github.com>
2024-03-09 14:12:00 -0500

    added type to prettier config

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,6 +2,7 @@
 
 const pentible = require("@pentible/prettier");
 
+/** @type {import('prettier').Config} */
 module.exports = {
     ...pentible,
     plugins: [...pentible.plugins, "prettier-plugin-tailwindcss"],
