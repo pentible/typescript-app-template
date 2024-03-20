b24383ce85617a577f2c3b5984fbfc616f0089de
William Villeneuve <ciiqr@users.noreply.github.com>
2024-03-19 21:24:04 -0400

    moved web/desktop tailwind configs to cjs for consistency

--- apps/desktop/tailwind.config.cjs
+++ apps/desktop/tailwind.config.cjs
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
     content: ["./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {
@@ -11,4 +10,4 @@ export default {
         },
     },
     plugins: [],
-} satisfies Config;
+};
