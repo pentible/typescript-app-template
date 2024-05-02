d4d3b6951e76a7d51ceb82a226dc888c7c641d85
William Villeneuve <ciiqr@users.noreply.github.com>
2024-05-02 18:13:48 -0400

    added example prisma migration

---
+++ prisma/migrations/20240502221317_example/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Example" (
+    "id" VARCHAR(24) NOT NULL,
+    "title" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Example_pkey" PRIMARY KEY ("id")
+);
