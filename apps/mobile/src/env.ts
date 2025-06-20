4874b7ae47b692e7e5a56a58c90017bc9f3370ec
William Villeneuve <ciiqr@users.noreply.github.com>
2025-06-20 13:02:35 -0400

    expo: updated env docs

--- apps/mobile/src/env.ts
+++ apps/mobile/src/env.ts
@@ -3,13 +3,13 @@ import { z } from "zod";
 
 export const env = createEnv({
     clientPrefix: "EXPO_PUBLIC_",
-    // NOTE: shared should only be used for "default" env vars available everywhere
     // NOTE: client is for public env vars, available on the client and the server
     client: {
         EXPO_PUBLIC_APP_URL: z.string().url(),
     },
-    // NOTE: client side env vars must be directly referenced from `process.env`
-    // for the nextjs compiler to properly inline them
+    // NOTE: env vars must be directly referenced from `process.env.*` for the
+    // expo compiler to properly inline them.
+    // SEE: https://docs.expo.dev/guides/environment-variables/
     runtimeEnvStrict: {
         EXPO_PUBLIC_APP_URL: process.env.EXPO_PUBLIC_APP_URL,
     },
