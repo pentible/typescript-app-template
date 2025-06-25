db0fb23a9b77ae2e18ca31789c9e0fdefebe24fc
William Villeneuve <ciiqr@users.noreply.github.com>
2025-06-25 14:29:09 -0400

    expo: separate not found file no longer required because the new +not-found filename doesn't confure nativewind

--- apps/mobile/src/app/+not-found.tsx
+++ apps/mobile/src/app/+not-found.tsx
@@ -1,11 +1,21 @@
-import { Stack } from "expo-router";
-import { NotFound } from "#src/components/not-found";
+import { Stack, Link } from "expo-router";
+import { Text, View } from "react-native";
 
 export default function NotFoundScreen() {
     return (
         <>
             <Stack.Screen options={{ title: "Oops!" }} />
-            <NotFound />
+            <View className="flex-1 items-center justify-center p-5">
+                <Text className="font-sans text-xl">
+                    This screen doesn&apos;t exist.
+                </Text>
+
+                <Link href="/" className="mt-4 py-4">
+                    <Text className="font-sans text-sm text-blue-500">
+                        Go to home screen!
+                    </Text>
+                </Link>
+            </View>
         </>
     );
 }
