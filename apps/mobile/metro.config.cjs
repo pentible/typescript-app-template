68547efd70aa042cd0b258b40a804aeae0cb50bf
William Villeneuve <ciiqr@users.noreply.github.com>
2026-08-02 17:42:14 -0400

    updated to expo v54

--- apps/mobile/metro.config.cjs
+++ apps/mobile/metro.config.cjs
@@ -1,26 +1,10 @@
 "use strict";
 
-// Learn more: https://docs.expo.dev/guides/monorepos/
-const path = require("path");
 const { getDefaultConfig } = require("@expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, "../..");
-
 // Create the default Metro config
-const config = getDefaultConfig(projectRoot);
-
-// TODO: should only watch package/* deps: https://docs.expo.dev/guides/monorepos/#1-why-do-we-need-to-watch
-// 1. Watch all files within the monorepo
-config.watchFolders = [workspaceRoot];
-// 2. Let Metro know where to resolve packages and in what order
-config.resolver.nodeModulesPaths = [
-    path.resolve(projectRoot, "node_modules"),
-    path.resolve(workspaceRoot, "node_modules"),
-];
-// 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
-config.resolver.disableHierarchicalLookup = true;
+const config = getDefaultConfig(__dirname);
 
 module.exports = withNativeWind(config, {
     input: "./src/styles/globals.css",
