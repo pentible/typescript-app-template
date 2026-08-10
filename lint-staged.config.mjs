e88190f0c0363fa552e86c325d400bdc5044de34
William Villeneuve <ciiqr@users.noreply.github.com>
2026-08-10 13:30:30 -0400

    prisma: generate when creating new migrations, and when committing

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -8,6 +8,8 @@ const config = {
     // NOTE: using the function syntax so files aren't passed to the command
     "**/*.{mjs,ts}": () => "tsc --noEmit",
     "package-lock.json": () => "check-package-lock",
+    "packages/backend/prisma/schema.prisma": () =>
+        "npx -w @repo/backend prisma generate --no-hints",
 };
 
 export default config;
